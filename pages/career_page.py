0f0d678dab44c151ece6c37ebada82b3030df168
dreamland1903 <evgenia.novikova1903@gmail.com>
2026-06-17 09:57:17 +0300

    add real code to files

---
+++ pages/career_page.py
@@ -0,0 +1,82 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
+
+from tests.conftest import driver
+
+
+class CareerPage(BasePage):
+    cookie_close_button=(By.ID,"cookie_msg_close")
+    region_button=(By.XPATH,'//button[@data-default="איזורים"]' )
+    areas_button=(By.XPATH,'//button[@data-default="תחומים"]')
+    sub_fields_button = (By.XPATH, '//button[@data-default="תתי תחומים"]')
+    search_input=(By.ID,"freeText")
+    search_button=(By.CSS_SELECTOR,"button.submit_search.green_btn")
+    no_results_message = (By.ID, "no_jobs_found_title")
+    all_jobs_link=(By.LINK_TEXT,"לכל המשרות")
+    job_cards = (By.XPATH, "//div[@id='jobs_list']/div[contains(@class, 'jobs_list_order_wrap')]")
+
+    item_in_open_dropdown_xpath = "//ul[contains(@class, 'jobs_index_dropdown')]//*[text()='{0}' or contains(text(), '{0}')]"
+
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    def close_cookie_banner(self):
+        self.click_element(self.cookie_close_button)
+
+    def open_regions(self):
+        self.click_element(self.region_button)
+
+    def open_areas(self):
+        self.click_element(self.areas_button)
+
+    def open_subfields(self):
+        self.click_element(self.sub_fields_button)
+
+    def enter_search_text(self,text):
+        self.input_text(self.search_input,text)
+
+    def click_search(self):
+        self.click_element(self.search_button)
+
+    def select_item_with_scroll(self, item_name):
+        xpath=self.item_in_open_dropdown_xpath.format(item_name)
+        target_element = self.wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", target_element)   #Java
+        target_element.click()
+
+    def get_no_results_text(self):
+        return self.get_element_text(self.no_results_message)
+
+    def click_all_jobs_link(self):
+        self.click_element(self.all_jobs_link)
+
+    def cards_list_visible(self):
+        try:
+            cards = self.find_elements(self.job_cards)
+            return len(cards)
+        except:
+            return 0
+
+    def check_text_in_all_results(self,expected_text):
+        cards= self.find_elements(self.job_cards)
+        if not cards:
+            return False
+        for card in cards:
+            card_text=card.text.strip()
+            if not card_text:
+                continue
+            if expected_text not in card.text and "ארצי" not in card.text:
+                print("\n=== НАЙДЕНА НЕОЖИДАННАЯ КАРТОЧКА ===")
+                print(card.text)
+                print("====================================\n")
+                return False
+        return True
+
+
+
+
+
+
+
+
---
+++ tests/test_career.py
@@ -0,0 +1,135 @@
+from pages.career_page import CareerPage
+
+def test_career_filters(driver):
+    career_page=CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+
+    career_page.open_regions()
+    career_page.select_item_with_scroll("מרכז")
+    career_page.open_regions()
+
+    career_page.open_areas()
+    career_page.select_item_with_scroll("כספים")
+    career_page.open_areas()
+
+    career_page.open_subfields()
+    career_page.select_item_with_scroll("עוזר חשב מתחיל")
+    career_page.open_subfields()
+
+    career_page.enter_search_text("כלכלה")
+    career_page.click_search()
+
+def test_career_search_no_results(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+    career_page.enter_search_text("qwerty")
+    career_page.click_search()
+
+    error_text=career_page.get_no_results_text()
+    assert "לא נמצא" in error_text
+
+    career_page.click_all_jobs_link()
+    assert "user_page=1&freeText=gvgvxfgf" not in driver.current_url
+
+def test_career_search_special_characters(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+    career_page.enter_search_text("@#$%^&*()")
+    career_page.click_search()
+
+    error_text=career_page.get_no_results_text()
+    assert "לא נמצא" in error_text
+
+def test_empty_career_search(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+    career_page.enter_search_text("   ")
+    career_page.click_search()
+
+    assert career_page.cards_list_visible() > 0
+
+def test_long_conversational_search(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+    career_page.enter_search_text("אני רוצה משהו שקשור למחשבים ואפשר לעבוד מהבית כמה פעמים בשבוע")
+    career_page.click_search()
+
+    error_text = career_page.get_no_results_text()
+    assert "לא נמצא" in error_text
+
+
+def test_input_search_hebrew_keyword(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+    career_page.close_cookie_banner()
+    search_word="מרכז"
+    career_page.enter_search_text("מרכז")
+    career_page.click_search()
+
+    assert career_page.cards_list_visible()>0
+
+def test_career_search_english_keyword(driver):
+    career_page = CareerPage(driver)
+    driver.get("https://www.strauss-group.co.il/career/")
+    driver.maximize_window()
+
+    career_page.close_cookie_banner()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
